9e2698b6a1d7b85cf120ca34dffaac3ea338c6cd
laura <you@example.com>
2024-08-20 12:27:42 -0500

    Hi there!

---
+++ ContinuedStatement/src/continueStatement/ContinuedStatement.java
@@ -0,0 +1,15 @@
+package continueStatement;
+
+public class ContinuedStatement {
+
+	public static void main(String[] args) {
+		for (int a = 1; a <= 17; a++) {
+			if(a==16) {
+				continue;
+			}
+			System.out.println(a);
+		}
+
+	}
+
+}
